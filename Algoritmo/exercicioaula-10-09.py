84ccf105f6861d35dd94f5f6d5ee374b61c422d1
Raphael Oliveira <166321121+MacNoosh@users.noreply.github.com>
2024-09-10 21:32:35 -0300

    Update exercicioaula-10-09.py

--- Algoritmo/exercicioaula-10-09.py
+++ Algoritmo/exercicioaula-10-09.py
@@ -1,9 +1,10 @@
+
 def potencia(base, expoente):
     if expoente == 0:
         return 1
     return base * potencia(base, expoente - 1)
-resultado_potencia = potencia(2, 3)
-print(f"2^3 = {resultado_potencia}")
+resultado_potencia = potencia(5, 3)
+print(f"resultado = {resultado_potencia}")
 
 
 def fibonacci(n):
@@ -12,16 +13,16 @@ def fibonacci(n):
     elif n == 1:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
-resultado_de_fibonacci = fibonacci(1)
-print(f"O 5º termo da sequência de Fibonacci é {resultado_de_fibonacci}")
+resultado_de_fibonacci = fibonacci(8)
+print(f"O termo escolhido da sequência de Fibonacci é {resultado_de_fibonacci}")
 
 
 def contar_digitos(n):
     if n < 10:
         return 1
     return 1 + contar_digitos(n // 10)
-resultado_contar_digitos = contar_digitos(12345)
-print(f"O número 12345 tem {resultado_contar_digitos} dígitos")
+resultado_contar_digitos = contar_digitos(12345678)
+print(f"O número informado tem {resultado_contar_digitos} dígitos")
 
 
 def palindromo(s):
@@ -30,9 +31,10 @@ def palindromo(s):
     if s[0] != s[-1]:
         return False
     return palindromo(s[1:-1])
-resultado_do_palindromo_1 = palindromo("Renner")
+resultado_do_palindromo_1 = palindromo("ola")
 resultado_do_palindromo_2 = palindromo("osso")
 print(f"'Renner' é um palíndromo? {resultado_do_palindromo_1}")
 print(f"'ossos' é um palíndromo? {resultado_do_palindromo_2}")
+
 
 
